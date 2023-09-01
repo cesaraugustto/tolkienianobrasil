fc85acd7331c5991bcbc2a67cab05d50acecf612
Cesar Augusto <102271334+cesaraugustto@users.noreply.github.com>
2023-08-31 21:14:11 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -52,8 +52,12 @@ const iconMap = {
   Region: yellowIcon
 };
 
+const markers = [];
+const searchInput = document.getElementById('search-input');
+
+
 function createMarkers(places) {
-  const markers = [];
+
 
   places.forEach(place => {
     const coords = place.coords.split(',');
@@ -64,21 +68,22 @@ function createMarkers(places) {
 
       const marker = L.marker([lat, lng], { icon: iconMap[place.type] });
       marker.type = place.type;
+      marker.options.place = place; // Adicione a propriedade place ao marcador
 
-      marker.on("click", function() {
+      marker.on("click", function () {
         var pos = map.latLngToLayerPoint(marker.getLatLng());
         pos.y -= 15;
         var fx = new L.PosAnimation();
 
-        fx.once('end',function() {
-            pos.y += 15;
-            fx.run(marker._icon, pos, 0.8);
+        fx.once('end', function () {
+          pos.y += 15;
+          fx.run(marker._icon, pos, 0.8);
         });
 
         fx.run(marker._icon, pos, 0.3);
       });
 
-      marker.on('click', function() {
+      marker.on('click', function () {
         const content = `
           <div>
             <h2>${place.name}</h2>
@@ -88,9 +93,9 @@ function createMarkers(places) {
         showModal(content);
       });
       markers.push(marker);
-    } 
+    }
   });
-return markers;
+  return markers;
 }
 
 //I stopped here on the improvements
@@ -127,14 +132,13 @@ function showModal(content) {
 }
 
 
-
-
 // Função para mostrar os marcadores no mapa
 function showMarkers(markers) {
   markers.forEach(marker => {
     marker.addTo(map); // Adiciona o marcador ao mapa
   });
 }
+
 
 // Função para ocultar todos os marcadores do mapa
 function hideMarkers(markers) {
@@ -143,54 +147,71 @@ function hideMarkers(markers) {
   });
 }
 
+// Função para filtrar marcadores com base no nome
+function filterMarkers(searchTerm) {
+  markers.forEach(marker => {
+    const placeName = marker.options.place.name.toLowerCase();
+    const shouldShow = placeName.includes(searchTerm);
+
+    if (shouldShow) {
+      marker.addTo(map); // Mostra o marcador
+    }
+  });
+}
+
+
+
+// Adicionar evento de input para a pesquisa
+searchInput.addEventListener('input', function () {
+  const searchTerm = searchInput.value.trim().toLowerCase();
+
+  // Ocultar todos os marcadores antes de filtrar
+  hideMarkers(markers);
+
+  if (searchTerm === "") {
+    // Mostrar todos os marcadores se a pesquisa estiver vazia
+    showMarkers(markers);
+  } else {
+    // Filtrar marcadores com base na pesquisa
+    filterMarkers(searchTerm);
+  }
+});
+
+
+const filterCheckboxes = document.querySelectorAll('.filter-checkbox');
+
+filterCheckboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', function (event) {
+    const checkbox = event.target;
+
+    const filterType = checkbox.value;
+    if (checkbox.checked) {
+      // Mostrar marcadores do tipo selecionado
+      showMarkers(markers.filter(marker => marker.options.place.type === filterType));
+    } else {
+      // Ocultar marcadores do tipo desmarcado
+      hideMarkers(markers.filter(marker => marker.options.place.type === filterType));
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Carregar e exibir os marcadores ao abrir a página
 fetchJSON()
   .then(places => {
-    const markers = createMarkers(places);
+    markers.push(...createMarkers(places)); // Adicionar marcadores à variável global
     showMarkers(markers);
-
-    // Tratamento do evento de mudança no filtro
-    document.getElementById('CityFilter').addEventListener('change', function (event) {
-      const checkbox = event.target;
-
-      if (checkbox.checked) {
-        showMarkers(markers.filter(marker => marker.type === checkbox.value));
-      } else {
-        hideMarkers(markers.filter(marker => marker.type === checkbox.value));
-      }
-    });
-    // Tratamento do evento de mudança no filtro
-    document.getElementById('StructureFilter').addEventListener('change', function (event) {
-      const checkbox = event.target;
-
-      if (checkbox.checked) {
-        showMarkers(markers.filter(marker => marker.type === checkbox.value));
-      } else {
-        hideMarkers(markers.filter(marker => marker.type === checkbox.value));
-      }
-    });
-    // Tratamento do evento de mudança no filtro
-    document.getElementById('MountainFilter').addEventListener('change', function (event) {
-      const checkbox = event.target;
-
-      if (checkbox.checked) {
-        showMarkers(markers.filter(marker => marker.type === checkbox.value));
-      } else {
-        hideMarkers(markers.filter(marker => marker.type === checkbox.value));
-      }
-    });
-    // Tratamento do evento de mudança no filtro
-    document.getElementById('RegionFilter').addEventListener('change', function (event) {
-      const checkbox = event.target;
-
-      if (checkbox.checked) {
-        showMarkers(markers.filter(marker => marker.type === checkbox.value));
-      } else {
-        hideMarkers(markers.filter(marker => marker.type === checkbox.value));
-      }
-    });
-
-
   })
   .catch(error => {
     console.error('Erro ao carregar o arquivo JSON:', error);
@@ -203,10 +224,3 @@ map.invalidateSize();
 var mapContainer = document.getElementById('map');
 mapContainer.style.height = '100vh';
 mapContainer.style.width = '100%';
-
-
-
-
-
-
-
