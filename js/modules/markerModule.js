c02106d42fd9812fe8790f35138867577575ac1a
Cesar Augusto <cesar.atadeu@gmail.com>
2023-10-29 15:02:26 -0300

    first view loads only necessary markers

--- js/modules/markerModule.js
+++ js/modules/markerModule.js
@@ -2,10 +2,9 @@ import { showDescriptionModal } from "./modalModule.js";
 import { map } from "./mapModule.js";
 
 const markers = [];
-const loadedMarkers = [];
 const filterCheckboxes = document.querySelectorAll('.filter-checkbox');
 const searchInput = document.getElementById('search-input');
-let activeFilters = [];
+let isLoadingMarkers = false;
 
 function createCustomIcon(iconUrl) {
     return L.icon({
@@ -24,36 +23,6 @@ export const greenIcon = createCustomIcon('./img/iconGreen.png');
 export const yellowIcon = createCustomIcon('./img/iconYellow.png');
 export const iconMap = { Structure: redIcon, Mountain: blueIcon, City: greenIcon, Region: yellowIcon };
 
-function handleMoveEnd() {
-    const visibleBounds = map.getBounds();
-  
-    markers.forEach(marker => {
-      if (isVisibleOnMap(marker, visibleBounds)) {
-        const typeMatch = activeFilters.includes(marker.type);
-        const searchMatch = searchInput.value.trim() === '' || marker.options.place.name.toLowerCase().includes(searchInput.value.trim().toLowerCase());
-        const shouldShow = typeMatch && searchMatch;
-  
-        if (shouldShow) {
-          if (!loadedMarkers.includes(marker)) {
-            marker.addTo(map);
-            loadedMarkers.push(marker);
-          }
-        } else {
-          if (loadedMarkers.includes(marker)) {
-            marker.removeFrom(map);
-            loadedMarkers.splice(loadedMarkers.indexOf(marker), 1);
-          }
-        }
-      } else {
-        if (loadedMarkers.includes(marker)) {
-          marker.removeFrom(map);
-          loadedMarkers.splice(loadedMarkers.indexOf(marker), 1);
-        }
-      }
-    });
-  }
-  
-
 function isVisibleOnMap(marker, bounds) {
     return bounds.contains(marker.getLatLng());
 }
@@ -71,7 +40,7 @@ export function createMarkers(places) {
             marker.options.place = place;
 
             marker.on("click", function () {
-                var pos = map.latLatLngToLayerPoint(marker.getLatLng());
+                var pos = map.latLngToLayerPoint(marker.getLatLng());
                 pos.y -= 15;
                 var fx = new L.PosAnimation();
 
@@ -81,72 +50,67 @@ export function createMarkers(places) {
                 });
 
                 fx.run(marker._icon, pos, 0.3);
-            });
 
-            marker.on('click', function () {
-                const content = `<div> <h2>${place.name}</h2> <p>${place.description}</p> </div> `;
+                const content = `<div> <h2>${place.name}</h2> <p>${place.description}</p> </div>`;
                 showDescriptionModal(content);
             });
+
             markers.push(marker);
         }
     });
 
-    // Registre o ouvinte de evento para o movimento do mapa
     map.on('moveend', handleMoveEnd);
-
-    // Registre os ouvintes de eventos para os filtros de tipo
     filterCheckboxes.forEach(checkbox => {
-        checkbox.addEventListener('change', function () {
-            handleFilterChange();
-        });
+        checkbox.addEventListener('change', handleFilterChange);
     });
-
-    // Registre o ouvinte de evento para a pesquisa por texto
-    searchInput.addEventListener('input', function () {
-        handleFilterChange();
-    });
-
-    handleFilterChange();
+    searchInput.addEventListener('input', handleFilterChange);
+    handleMoveEnd(); // Chamando handleMoveEnd() inicialmente para carregar os marcadores visíveis.
 
     return markers;
 }
 
-export function showMarkers(markers) {
+export function showMarkers() {
     markers.forEach(marker => {
-        if (isVisibleOnMap(marker, map.getBounds()) && activeFilters.includes(marker.type)) {
+        if (isVisibleOnMap(marker, map.getBounds()) && filterMatch(marker.type) && searchMatch(marker)) {
             marker.addTo(map);
-            loadedMarkers.push(marker);
         }
     });
 }
 
-export function hideMarkers(markers) {
+export function hideMarkers() {
     markers.forEach(marker => {
-        if (loadedMarkers.includes(marker)) {
-            marker.removeFrom(map);
-            loadedMarkers.splice(loadedMarkers.indexOf(marker), 1);
-        }
+        marker.removeFrom(map);
     });
+}
+
+function filterMatch(type) {
+    const checkedCheckboxes = Array.from(filterCheckboxes).filter(checkbox => checkbox.checked);
+    return checkedCheckboxes.length === 0 || checkedCheckboxes.some(checkbox => checkbox.value === type);
 }
 
 function handleFilterChange() {
-    activeFilters = Array.from(filterCheckboxes)
-        .filter(checkbox => checkbox.checked)
-        .map(checkbox => checkbox.value);
+    isLoadingMarkers = true;
+    hideMarkers();
 
-    const searchTerm = searchInput.value.trim().toLowerCase();
+    setTimeout(() => {
+        showMarkers();
+        isLoadingMarkers = false;
+    }, 100);
+}
 
+function handleMoveEnd() {
     markers.forEach(marker => {
-        const typeMatch = activeFilters.includes(marker.type);
-        const searchMatch = searchTerm === "" || marker.options.place.name.toLowerCase().includes(searchTerm);
-        const shouldShow = typeMatch && searchMatch;
-
-        if (shouldShow && !loadedMarkers.includes(marker)) {
+        if (isVisibleOnMap(marker, map.getBounds()) && filterMatch(marker.type) && searchMatch(marker)) {
             marker.addTo(map);
-            loadedMarkers.push(marker);
-        } else if (!shouldShow && loadedMarkers.includes(marker)) {
+        } else {
             marker.removeFrom(map);
-            loadedMarkers.splice(loadedMarkers.indexOf(marker), 1);
         }
     });
+
+    isLoadingMarkers = false;
+}
+
+function searchMatch(marker) {
+    const searchTerm = searchInput.value.trim().toLowerCase();
+    return searchTerm === '' || marker.options.place.name.toLowerCase().includes(searchTerm);
 }
